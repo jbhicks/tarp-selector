e7580393853488dbd06fbc03b17bfc2b993f1ee6
Joshua Hicks <jbhicks11b@gmail.com>
2024-06-03 22:54:47 -0700

    added new skeleton sections

--- tarpSelector_templ.go
+++ tarpSelector_templ.go
@@ -43,6 +43,14 @@ func tarpSelector() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
+		templ_7745c5c3_Err = fastenerSelector().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = notesAndInstructions().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		templ_7745c5c3_Err = footer().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
